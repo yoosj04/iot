cf6949fa2ff068b5429f422a8c33c9261ca55ed2
asdf <yoosj04@naver.com>
2018-01-22 14:25:48 +0900

    2

---
+++ lunch.py
@@ -0,0 +1,7 @@
+# 실습2. 점심메뉴추천 챗봇 만들기
+import random
+# 1. 점심메뉴들이 담긴 lunch (리스트) 배열 만들기
+lunch = [ "돈카츠", "참치회", "성게알밥", "스테이크" ]
+# 아래의 코드는 수정하지 않으셔도 됩니다.
+menu = random.choice(lunch)
+print('오늘 점심은 ' + menu)
